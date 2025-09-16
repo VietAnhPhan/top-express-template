49c377ad6375314879d98bdc937d8da3784681f1
Viet Anh Phan <vietanhphan2810@gmail.com>
2025-09-16 18:19:27 +0700

    Add CRUD operations for User model - Implement routes correspond to CRUD operations of User model - Implement controllers correspond to CRUD operations of User model

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,35 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const { param, validationResult } = require("express-validator");
+const passport = require("passport");
+
+const userController = require("../controllers/userController");
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
+router.use(passport.authenticate("jwt", { session: false }));
 
-router.get('/:userId', (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
+const sendValidationResults = (req, res, next) => {
+  const validations = validationResult(req);
+  if (!validations.isEmpty()) {
+    res.status(400).json({
+      errors: validations.array(),
+    });
+  }
+  next();
+};
+
+router.use(
+  "/:id",
+  param("id").isNumeric().withMessage("User Id should be a number"),
+  sendValidationResults
+);
+
+router.get("/:id", userController.getUser);
+
+router.put("/:id", userController.updateUser);
+
+router.delete("/:id", userController.deleteUser);
+
+router.get("/", userController.getAllUser);
 
 module.exports = router;
